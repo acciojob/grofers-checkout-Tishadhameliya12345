cdbcc07ee4856efcd67f883cf6395d5bc404d28a
Tishadhameliya12345 <159407986+Tishadhameliya12345@users.noreply.github.com>
2025-02-14 11:50:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,25 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	const prices = document.querySelectorAll(".price");
+
+    let total = 0;
+	
+    for (let i = 0; i < prices.length; i++) {
+        total = total + parseFloat(prices[i].textContent); 
+    }
+
+	const table = document.querySelector("table");
+    const totalRow = document.createElement("tr");
+    const totalCell = document.createElement("td");
+
+	totalCell.colSpan = 2;
+    totalCell.textContent = `Total Price: Rs ${total}`;
+
+	totalRow.appendChild(totalCell);
+    table.appendChild(totalRow);
+
+	getSumBtn.disabled = true;
   
 };
 
